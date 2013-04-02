f463dafcdfa9adaf53c20726db1eb96ea6e7e68e
Pete Moore <pmoore@mozilla.com>
2013-04-02 16:10:12 +0200

    bug628796: Some improvements to check size of download is correct, and to output the canonical url for download

--- release/get-update-xml.sh
+++ release/get-update-xml.sh
@@ -7,7 +7,7 @@ update_xml="$(mktemp -t update.xml.XXXXXX)"
 curl --retry 5 --retry-max-time 30 -k -s -L "${update_url}" > "${update_xml}" || echo "FAILURE: Could not retrieve http header for update.xml file from ${update_url}" >> "${failures}"
 for patch_type in ${patch_types}
 do  
-    mar_url="$(cat "${update_xml}" | sed -n 's/.*<patch .*type="'"${patch_type}"'".* URL="\([^"]*\)".*/\1/p' | sed 's/\&amp;/\&/g')"
-    [ -z "${mar_url}" ] && echo "FAILURE: No patch type '${patch_type}' found in update.xml from ${update_url}" >> "${failures}" || echo "${mar_url}" "${failures}"
+    mar_url_and_size="$(cat "${update_xml}" | sed -n 's/.*<patch .*type="'"${patch_type}"'".* URL="\([^"]*\)".*size="\([^"]*\)".*/\1 \2/p' | sed 's/\&amp;/\&/g')"
+    [ -z "${mar_url_and_size}" ] && echo "FAILURE: No patch type '${patch_type}' found in update.xml from ${update_url}" >> "${failures}" || echo "${mar_url_and_size}" "${failures}"
 done
 rm "${update_xml}"
