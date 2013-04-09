e234058d2fe07cc172cfd901e04beba84d2f3acd
Pete Moore <pmoore@mozilla.com>
2013-04-09 13:56:49 +0200

    Fixed problem with HTTP 503 errors, where the mar file size was getting multiple Content-Length entries from the http header, rather than only taking the last one in the header - now only takes last. Also will work the same with update.xml files - if multiple entries for a patch type are found, will only take last one in update.xml file

--- release/test-mar-url.sh
+++ release/test-mar-url.sh
@@ -10,8 +10,8 @@ curl --retry 5 --retry-max-time 30 -k -s -I -L "${mar_url}" > "${mar_header_file
 mar_file_curl_exit_code=$?
 
 # check file size matches what was written in update.xml
-mar_actual_size="$(sed -e "s/$(printf '\r')//" -n -e 's/^Content-Length: //p' "${mar_header_file}")"
-mar_actual_url="$(sed -e "s/$(printf '\r')//" -n -e 's/^Location: //p' "${mar_header_file}")"
+mar_actual_size="$(sed -e "s/$(printf '\r')//" -n -e 's/^Content-Length: //p' "${mar_header_file}" | tail -1)"
+mar_actual_url="$(sed -e "s/$(printf '\r')//" -n -e 's/^Location: //p' "${mar_header_file}" | tail -1)"
 [ -n "${mar_actual_url}" ] && mar_url_with_redirects="${mar_url} => ${mar_actual_url}" || mar_url_with_redirects="${mar_url}"
 
 if [ "${mar_actual_size}" == "${mar_required_size}" ]
