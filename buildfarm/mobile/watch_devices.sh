d13867f1a32f8a2cee7fb7131fb8b6dd9856ae66
Pete Moore <pmoore@mozilla.com>
2013-05-08 15:43:24 +0200

    bug869897: First pass of changes

--- buildfarm/mobile/watch_devices.sh
+++ buildfarm/mobile/watch_devices.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -eu
-PYTHON=`which python`
 
 # MAGIC NUMBERS (global)
 # used to determine how long we sleep when...
@@ -7,31 +6,12 @@ CYCLE_WAIT=300 # 5 minutes between cycling of watches for our devices
 SUCCESS_WAIT=200 # ... seconds after we startup buildbot
 FAIL_WAIT=500 # ... seconds after we stop buildbot due to error.flg
 
-# boilerplate
-warn() { for m; do echo "$m"; done 1>&2; }
-die() { warn "$@" >&2 ; exit 1 ; }
-usage() { warn "$@" "${USAGE:-}" ; test $# -eq 0; exit $?; }
-
 log() {
-  local ts=`date +"%Y-%m-%d %H:%M:%S"`
-  warn "$ts -- $@" ;
+  echo "$(date +"%Y-%m-%d %H:%M:%S") -- ${1}" >&2
 }
-debug() { true; } # No actual debug logging yet
 death() {
-  local ts=`date +"%Y-%m-%d %H:%M:%S"`
-  die "$ts -- $@" ;
-}
-
-function log_output_to() {
-  # $1 is "where to
-  # $2 (if present) is "hide output to invoker"
-  if [ ! -z "${2:-}" ]; then
-    # Redirect all output
-    exec >>$1 2>&1
-  else
-    # Tee all output instead
-    exec > >(tee -a $1) 2>&1
-  fi
+  log "${1}"
+  exit "${2}"
 }
 
 function check_buildbot_running() {
@@ -43,40 +23,18 @@ function check_buildbot_running() {
   fi
   local expected_pid=`cat /builds/$device/twistd.pid`
   debug "buildbot pid is $expected_pid"
-  local retcode=0
-  kill -0 $expected_pid 2>&1 > /dev/null || retcode=$?
-  return $retcode
-}
-
-function kill_lockfile() {
-  # $1 is device name
-  rm -f /builds/$1/watcher.lock
-}
-
-function device_check_exit() {
-  kill_lockfile $1
-  log "Cycle for our device ($1) complete"
+  kill -0 $expected_pid 2>&1 >/dev/null
+  return $?
 }
 
 function device_check() {
   local device=$1
-  log_output_to "/builds/$device/watcher.log" hide
   export PYTHONPATH=/builds/sut_tools
   deviceIP=`python -c "import sut_lib;print sut_lib.getIPAddress('$device')" 2> /dev/null`
-  local retcode=0
-  lockfile -r0 /builds/$device/watcher.lock 2>&1 > /dev/null || retcode=$?
-  if [ $retcode -ne 0 ]; then
-    die "failed to aquire lockfile"
-  fi
   log "Starting cycle for our device ($device) now"
-  # Trap here, not earlier so that if lockfile fails, we don't clear the lock
-  # From another process
-  trap "device_check_exit $device" EXIT
-  local buildbot_running=0
-  check_buildbot_running $device || buildbot_running=$?
-  if [ $buildbot_running -ne 0 ]; then
+  if check_buildbot_running; then
     if [ -f /builds/$device/disabled.flg ]; then
-       death "Not Starting due to disabled.flg"
+       death "Not Starting due to disabled.flg" 64
     fi
     if [ -f /builds/$device/error.flg ]; then
       # Clear flag if older than an hour
@@ -84,19 +42,19 @@ function device_check() {
         log "removing $device error.flg and trying again"
         rm -f /builds/$device/error.flg
       else
-        death "Error Flag told us not to start"
-      fi;
+        death "Error Flag told us not to start" 65
+      fi
     fi
     export SUT_NAME=$device
     export SUT_IP=$deviceIP
     retcode=0
-    $PYTHON /builds/sut_tools/verify.py $device || retcode=$?
+    python /builds/sut_tools/verify.py $device || retcode=$?
     if [ $retcode -ne 0 ]; then
        if [ ! -f /builds/$device/error.flg ]; then
            echo "Unknown verify failure" | tee "/builds/$device/error.flg"
        fi
-       death "Verify failed"
-    fi;
+       death "Verify failed" 66
+    fi
     /builds/tools/buildfarm/mobile/manage_buildslave.sh start $device
     log "Sleeping for ${SUCCESS_WAIT} sec after startup, to prevent premature flag killing"
     sleep ${SUCCESS_WAIT} # wait a bit before checking for an error flag or otherwise
@@ -113,22 +71,20 @@ function device_check() {
         set -e
         log "sleeping for ${FAIL_WAIT} seconds after killing, to prevent startup before master notices"
         sleep ${FAIL_WAIT} # Wait a while before allowing us to turn buildbot back on
-    fi;
-  fi;
-  exit
+    fi
+  fi
+  log "Cycle for our device ($device) complete"
 }
 
 
 function watch_launcher(){
   echo "STARTING Watcher"
-  log_output_to /builds/watcher.log
-  # get full abs path to our script for nested call
-  local script_abs_path=$(cd $(dirname $0);/bin/pwd)/${0##*/}
   while true; do
     log "Watcher is still alive"
-    for d in `ls -d tegra-*[0-9] panda-*[0-9] 2> /dev/null`; do
+    ls -d /builds/{tegra-*[0-9],panda-*[0-9]} 2>/dev/null | sed 's:.*/::' \ # strip device name from full path
+    | while read d; do
       log "..checking $d"
-      $script_abs_path $d &
+      "${0}" "${d}" &
     done
     log "..done next run in ${CYCLE_WAIT} seconds."
     sleep ${CYCLE_WAIT}
@@ -136,7 +92,10 @@ function watch_launcher(){
 }
 
 if [ "$#" -eq 0 ]; then
-   watch_launcher;
+  watch_launcher 2>&1 | tee -a "/builds/watcher.log"
 else
-   device_check $1 ;
-fi;
+  (
+    flock -n 9 || death "lockfile for device ${1} locked by another process" 67
+    device_check "${1}"
+  ) 9>"/builds/${1}/watcher.lock" >"/builds/${1}/watcher.log" 2>&1
+fi
