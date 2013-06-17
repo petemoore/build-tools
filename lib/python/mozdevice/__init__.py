e1a0fdea531bd94d9ada805c4c4f80978d982492
Pete Moore <pmoore@mozilla.com>
2013-06-17 17:46:50 +0200

    Added mozlog etc

--- lib/python/mozdevice/__init__.py
+++ lib/python/mozdevice/__init__.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from devicemanager import DeviceManager, DMError
-from devicemanagerADB import DeviceManagerADB
-from devicemanagerSUT import DeviceManagerSUT
-from droid import DroidADB, DroidSUT, DroidConnectByHWID
+from mozdevice.devicemanager import DeviceManager, DMError
+from mozdevice.devicemanagerADB import DeviceManagerADB
+from mozdevice.devicemanagerSUT import DeviceManagerSUT
+from mozdevice.droid import DroidADB, DroidSUT, DroidConnectByHWID
