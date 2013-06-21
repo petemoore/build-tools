f07bab48f13bd06f6e76680b8831789d259423bb
Pete Moore <pmoore@mozilla.com>
2013-06-21 16:52:46 +0200

    bug850572: minor fix after some refactoring

--- sut_tools/tegra_powercycle.py
+++ sut_tools/tegra_powercycle.py
@@ -10,9 +10,9 @@ import sys
 import site
 site.addsitedir(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../lib/python"))
 
-from sut_lib import powermanagement
+from sut_lib import devices, powermanagement
 
-if len(powermanagement.tegras) == 0:
+if len(devices.tegras) == 0:
     print "error: The devices.json data file appears to be empty or not found."
     sys.exit(2)
 
@@ -23,7 +23,7 @@ if len(sys.argv[1:]) == 0:
 for tegra in sys.argv[1:]:
     if not tegra.lower().startswith('tegra-'):
         tegra = 'tegra-%s' % tegra
-    if not tegra in powermanagement.tegras:
+    if not tegra in devices.tegras:
         print "ERROR: %s not found in devices.json" % tegra
 
     powermanagement.reboot_device(tegra, debug=True)
