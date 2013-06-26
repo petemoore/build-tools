92415a5dbe4ab19a16a792c007c04a9047a1359a
Pete Moore <pmoore@mozilla.com>
2013-06-26 13:43:42 +0200

    simplified regular expression

--- buildfarm/mobile/watch_devices.sh
+++ buildfarm/mobile/watch_devices.sh
@@ -37,7 +37,7 @@ function device_check_exit() {
 function device_check() {
   local device=$1
   export PYTHONPATH=/builds/sut_tools
-  deviceIP="$(nslookup "${device}" 2>/dev/null | sed -n '/^Name/,$s/^Address:[[:space:]]*//p')"
+  deviceIP="$(nslookup "${device}" 2>/dev/null | sed -n '/^Name/,$s/^Address: *//p')"
   if ! lockfile -r0 "/builds/${device}/watcher.lock" >/dev/null 2>&1; then
     death "failed to aquire lockfile" 67
   fi
