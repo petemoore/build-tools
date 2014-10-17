6088687ef4f5999a2ceedbeaefb52c3766ffeead
Pete Moore <pmoore@mozilla.com>
2014-10-17 11:08:47 +0200

    Don't update bugzilla if only preparing reconfig

--- buildfarm/maintenance/end_to_end_reconfig.sh
+++ buildfarm/maintenance/end_to_end_reconfig.sh
@@ -445,7 +445,7 @@ commit_processing_options=()
 if [ "${UPDATE_WIKI}" == '1' ]; then
     commit_processing_options+=('--wiki-markup-file' "${RECONFIG_DIR}/${RECONFIG_UPDATE_FILE}")
 fi
-if [ "${UPDATE_BUGZILLA}" == '1' ]; then
+if [ "${UPDATE_BUGZILLA}" == '1' ] && [ "${PREPARE_ONY}" != '1' ]; then
     commit_processing_options+=('--update-bugzilla')
 fi
 
