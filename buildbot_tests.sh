a2f600d7a6a76a3ede60a2263f70829cb60f6de8
Pete Moore <pmoore@mozilla.com>
2015-01-08 14:12:42 +0100

    try

--- buildbot_tests.sh
+++ buildbot_tests.sh
@@ -22,6 +22,7 @@ hg -R "${TOX_WORK_DIR}/buildbot" checkout production-0.8
 hg -R "${TOX_WORK_DIR}/buildbotcustom" checkout production-0.8
 hg -R "${TOX_WORK_DIR}/buildbot-configs" checkout production
 cd "${TOX_WORK_DIR}/buildbot/master" && python setup.py install
+cd "${TOX_WORK_DIR}/buildbot-configs" && python setup.py install
 rm -rf "${TOX_INI_DIR}/test-output"
 rm -rf "${TOX_INI_DIR}/run/shm/buildbot"
 mkdir -p "${TOX_INI_DIR}/run/shm/buildbot"
